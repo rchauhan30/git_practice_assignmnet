1801731aa9c4fe9f4ed149cf8a9a43c6df34d4e2
rchauhan30 <rahul3030chauhan@gmail.com>
2023-01-08 21:37:00 +0530

    prime_problem_and_palindrome_is_done

---
+++ Palindrome_problem.js
@@ -0,0 +1,12 @@
+let str="naman"
+let stt ="";
+    for(i=str.length-1;i>=0; i++){
+ stt = stt + str[i];
+    }
+ if(stt == str){
+     console.log("Yes")
+     
+ }else{
+     console.log("No");
+     
+     }
